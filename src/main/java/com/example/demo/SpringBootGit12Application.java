54e6ddc9e977bd5fe34b4af571250bfee0dadeff
vumeran <=>
2021-03-11 22:03:29 +0530

    checkingfortemporarycomme

--- src/main/java/com/example/demo/SpringBootGit12Application.java
+++ src/main/java/com/example/demo/SpringBootGit12Application.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootGit12Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGit12Application.class, args);
+		ApplicationContext context = SpringApplication.run(SpringBootGit12Application.class, args);
+		
+		System.out.println("Application Started run");
 	}
 
 }
