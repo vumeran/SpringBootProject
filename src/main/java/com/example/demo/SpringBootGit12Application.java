c674652466248a5427991f677c7932edb2af5009
vumeran <=>
2021-03-11 21:17:42 +0530

    project initial commit

---
+++ src/main/java/com/example/demo/SpringBootGit12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGit12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGit12Application.class, args);
+	}
+
+}
